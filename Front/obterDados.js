85fb843759e06fa235af09632bebfe25b7f8352e
Thiago Damaceno <tavaresdamaceno@gmail.com>
2020-10-29 14:34:07 -0300

    Organizando funções em seus respectivos arquivos.

--- Front/obterDados.js
+++ Front/obterDados.js
@@ -15,38 +15,3 @@ function getDados() {
     response.then(data => criarCards(JSON.parse(data)))
 }
 
-function criarCards(data) {
-    console.log(data)
-    let elementos = ""
-    data.forEach((element) => {
-        if (element.nome != "" && element.descricao != "") {
-            elementos += 
-            `
-                <div class="card-container">
-                    <h4>${element.nome}</h4>
-                    <span>${element.descricao}</span>
-                </div>
-            `
-        }
-    })
-    document.querySelector('#container-apresentar').innerHTML = elementos
-}
-
-let abaAtiva = "Apresentar"
-
-document.querySelector('#btn-cadastrar').onclick = () => {
-    document.querySelector('#container-cadastrar').classList.remove('d-none')
-    document.querySelector('#container-apresentar').classList.add('d-none')
-
-    document.querySelector('#btn-cadastrar').classList.add('btn-aba-ativo')
-    document.querySelector('#btn-apresentar').classList.remove('btn-aba-ativo')
-}
-
-document.querySelector('#btn-apresentar').onclick = () => {
-    getDados()
-    document.querySelector('#container-cadastrar').classList.add('d-none')
-    document.querySelector('#container-apresentar').classList.remove('d-none')
-
-    document.querySelector('#btn-cadastrar').classList.remove('btn-aba-ativo')
-    document.querySelector('#btn-apresentar').classList.add('btn-aba-ativo')
-}
